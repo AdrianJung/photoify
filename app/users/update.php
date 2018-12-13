74ad6faa903c130b4a1c9d6e5316dda50af9f292
Adrian Jungnelius <a.jungnelius@gmail.com>
2018-12-13 11:20:33 +0100

    added app folder with database file, posts folder, users folder ith login, register and logout logic in php

---
+++ app/users/update.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__.'/../autoload.php';
+
+// checks if the user data is posted
+if (isset($_POST['newPassword'], $_POST['oldPassword']))
+{
+    // checks if passwords match
+    if ($_POST['newPassword'] != $_POST['oldPassword'])
+    {
+    $oldPassword = $_POST['oldPassword'];
+
+    $newPassword = password_hash($_POST['newPassword'], PASSWORD_DEFAULT);
+
+    $id = $_SESSION['user']['id'];
+
+    $statement = $pdo->prepare('SELECT password FROM users WHERE id = :id');
+
+    $statement->bindParam(':id', $id, PDO::PARAM_STR);
+
+    $statement->execute();
+
+    $password = $statement->fetch(PDO::FETCH_ASSOC);
+
+
+    if (password_verify($oldPassword, $password['password'])) {
+
+        $statement = $pdo->prepare('UPDATE users SET password = :password WHERE id = :id');
+
+        $statement->bindParam(':id', $id, PDO::PARAM_STR);
+
+        $statement->bindParam(':password', $newPassword, PDO::PARAM_STR);
+
+        $statement->execute();
+
+        echo "YOU UPDATED THE PASSWORD";
+    }
+
+    redirect('/index.php');
+    }
+};
