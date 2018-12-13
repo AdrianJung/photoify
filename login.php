0699189642c25ab4e2c36f2a494af747c44555b8
Adrian Jungnelius <a.jungnelius@gmail.com>
2018-12-13 11:27:38 +0100

    added login.php

---
+++ login.php
@@ -0,0 +1,14 @@
+<?php require __DIR__.'/views/header.php'; ?>
+<article>
+    <h1>Login</h1>
+    <form action="app/users/login.php" method="post">
+          <label for="username">Username</label>
+          <input class="" type="text" name="username" placeholder="Username" required>
+          <br>
+          <label for="password">Password</label>
+          <input class="" type="password" name="password" required>
+          <br>
+      <button type="submit" class="">Login</button>
+    </form>
+</article>
+<?php require __DIR__.'/views/footer.php'; ?>
