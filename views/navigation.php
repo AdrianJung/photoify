8bdbaee16cabeeb1a17744143a984bc1578196c3
Adrian Jungnelius <a.jungnelius@gmail.com>
2018-12-13 11:25:45 +0100

    added views folder with footer.php header.php and navigation.php

---
+++ views/navigation.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#"><?php echo $config['title']; ?></a>
+
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/index.php' ? 'active' : ''; ?>" href="/index.php">Home</a>
+        </li><!-- /nav-item -->
+        <?php if (!isset($_SESSION['user'])): ?>
+        <li class="nav-item">
+            <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/register.php' ? 'active' : ''; ?>" href="/register.php">Register</a>
+        </li><!-- /nav-item -->
+        <?php endif;?>
+        <li class="nav-item">
+            <?php if (isset($_SESSION['user'])): ?>
+                <a class="nav-link" href="/profile.php">Profile</a>
+                <a class="nav-link" href="/update.php">Update</a>
+                <a class="nav-link" href="/app/users/logout.php">Logout</a>
+            <?php else: ?>
+                <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/login.php' ? 'active' : ''; ?>" href="login.php">Login</a>
+            <?php endif; ?>
+        </li><!-- /nav-item -->
+    </ul><!-- /navbar-nav -->
+</nav><!-- /navbar -->
